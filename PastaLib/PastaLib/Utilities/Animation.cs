5c62c1f96efd0d3e526c0c43390e2dab272b6834
TastyWithPasta <tastywithpasta@gmail.com>
2013-05-30 00:10:35 +0100

    Added Animation/Interpolation

--- PastaLib/PastaLib/Utilities/Animation.cs
+++ PastaLib/PastaLib/Utilities/Animation.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace PastaGameLibrary.Utilities
+namespace PastaLib.Utilities
 {
 	public class Animation
 	{
--- PastaLib/PastaLib/Utilities/Interpolation.cs
+++ PastaLib/PastaLib/Utilities/Interpolation.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Timers;
 
-namespace PastaGameLibrary
+namespace PastaLib.Utilities
 {
 	public interface IPInterpolation<T>
 	{
